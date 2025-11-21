4c116392be979e585d35d0bc39f1b975d4c517d3
brunocoppola <bcoppola@udesa.edu.ar>
2025-11-21 13:13:28 -0300

    terminando logout

--- js/logout.js
+++ js/logout.js
@@ -1,1 +1,14 @@
- 
+ let recuperoStorage = localStorage.getItem("usuario")
+let login = document.querySelector(".menu2")
+let oculto = document.querySelector(".oculto")
+let logout = document.querySelector(".logout")
+
+logout.addEventListener('click', function(event){
+        sessionStorage.clear()
+     login.style.display = "line"
+     oculto.style.display = "none"
+     
+     
+    
+})
+
